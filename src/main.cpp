91d8030917e38bd6110f1ff4e9ba402632d691f1
guillem.cabre <guillem.cabre@estudiantat.upc.edu>
2024-10-07 12:36:53 +0200

    Made the main more modular

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include "graph.hpp"
+#include <functional>
 
 // Constants
-static constexpr int RGN = 1321312;
-static constexpr int MIN_NB_NODES = 10;
-static constexpr int MAX_NB_NODES = 100;
-static constexpr int NB_NODES_STEP = 10;
-static constexpr double RANDOM_GRAPH_P = 0.2; // 0 = no edges (empty graph), 1 = all edges (complete graph)
-static constexpr int TRIES_PER_P = 1000; // Number of tries for each value of p
-const std::string CSV_FILE = "data/results.csv";
+namespace Options {
+    static int RGN;
+    static int MIN_NB_NODES;
+    static int MAX_NB_NODES;
+    static int NB_NODES_STEP;
+    static int TRIES_PER_P; // Number of tries for each value of p
+    static std::string CSV_FILE = "data/results.csv";
+    static std::function<Graph(int)> GRAPH_GENERATOR;
+}
 
 // Struct for CSV entry
 struct CSVEntry {
@@ -37,28 +40,96 @@ void writeCSVEntry(std::ofstream& csvFile, const CSVEntry& entry) {
     csvFile << entry.toCSVString() << std::endl;
 }
 
+std::function<Graph(int)> selectGraphAlgorithm() {
+    std::string choice;
+
+    std::cout << "Select the graph generation algorithm:\n";
+    std::cout << "[Erdos-Renyi]\n";
+    std::cout << "[Square-Grid]\n";
+    std::cout << "[Triangular-Grid]\n";
+    std::cout << "[Random-Geometric]\n";
+    std::cout << "[Barabasi-Albert]\n";
+    std::cin >> choice;
+    
+    if (choice == "Erdos-Renyi") {
+        double p;
+        std::cout << "Enter p: ";
+        std::cin >> p;
+        return [p](int n) { return Graph::generateRandomGraph_ErdosRenyi(n, p); };
+    }
+    else if (choice == "Square-Grid") {
+        return [](int m) { return Graph::generateSquareGridGraph(m); };
+    }
+    else if (choice == "Triangular-Grid") {
+        return [](int rows) { return Graph::generateTriangularGridGraph(rows); };
+    }
+    else if (choice == "Random-Geometric") {
+        double r;
+        std::cout << "Enter r: ";
+        std::cin >> r;
+        return [r](int n) { return Graph::generateRandomGeometricGraph(n, r); };
+    }
+    else if (choice == "Barabasi-Albert") {
+        int m0, m;
+        std::cout << "Enter m0 and m: ";
+        std::cin >> m0 >> m;
+        return [m0, m](int n) { return Graph::generateRandom_BarabasiAlbert(n, m0, m); };
+    }
+    else {
+        std::cout << "Invalid choice\n";
+        return nullptr;
+    }
+}
+
+// Selects options
+void selectOptions() {
+    std::cout << "Enter the seed for the random number generator: ";
+    std::cin >> Options::RGN;
+
+    std::cout << "Enter the minimum number of nodes: ";
+    std::cin >> Options::MIN_NB_NODES;
+
+    std::cout << "Enter the maximum number of nodes: ";
+    std::cin >> Options::MAX_NB_NODES;
+
+    std::cout << "Enter the step for the number of nodes: ";
+    std::cin >> Options::NB_NODES_STEP;
+
+    std::cout << "Enter the number of tries per p: ";
+    std::cin >> Options::TRIES_PER_P;
+
+    std::cout << "Enter the path of the CSV file: ";
+    std::cin >> Options::CSV_FILE;
+
+    //Select the algorithm
+    Options::GRAPH_GENERATOR = selectGraphAlgorithm();
+}
+
 // Main function
-int main(int argc, char* argv[]) {
+int main() {
+    // Select options
+    selectOptions();
+
     // Seed the random number generator
-    srand(RGN);
+    srand(Options::RGN);
 
     // Write CSV header, first an info about the constants
-    std::ofstream csvFile(CSV_FILE);
+    std::ofstream csvFile(Options::CSV_FILE);
     writeCSVHeader(csvFile);
     
-    for (int n = MIN_NB_NODES; n <= MAX_NB_NODES; n += NB_NODES_STEP) {
+    for (int n = Options::MIN_NB_NODES; n <= Options::MAX_NB_NODES; n += Options::NB_NODES_STEP) {
         for (int step = 0; step <= 100; ++step) {
             // Calculate the value of q from the number of steps, to avoid losing precision
             double p = step * 0.01; //Probability of failure
 
             int connectedCount = 0;
-            for (int i = 0; i < TRIES_PER_P; ++i) {
+            for (int i = 0; i < Options::TRIES_PER_P; ++i) {
                 Graph G;
 
                 // Keep generating random graphs until you get a connected graph
                 while (true) {
-                    // Generate a random graph G(n, p)
-                    G = Graph::generateRandomGraph_ErdosRenyi(n, RANDOM_GRAPH_P);
+                    // Generate a random graph
+                    G = Options::GRAPH_GENERATOR(n);
 
                     // Check if the graph is connected, if not, continue
                     if (G.connectedComponents() == 1) break;
