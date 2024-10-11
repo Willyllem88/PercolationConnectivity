31f0c852bb880f705ce90d47cddac2cc9c18e36b
Willyllem88 <guillem_cabre@Willyllem88>
2024-10-11 13:09:29 +0200

    Paralelitzar tots els csv a plot, tot de cop

--- RScripts/plot_pn.R
+++ RScripts/plot_pn.R
@@ -1,23 +1,35 @@
-# Cargar el archivo CSV
-data <- read.csv("../data/vert.csv", sep=";")
-
-# Ver un resumen rápido de los datos
-head(data)
-
 # Instalar ggplot2 si no está instalado
 if (!require(ggplot2)) install.packages("ggplot2")
+if (!require(dplyr)) install.packages("dplyr")
 
-# Cargar ggplot2
+# Cargar las librerías necesarias
 library(ggplot2)
+library(dplyr)
 
-ggplot(data, aes(x = p, y = connectedGraphs, color = as.factor(n))) +
-  geom_line(size = 1) +  # Ajustar el grosor de la línea
-  geom_point(size = 2) + # Ajustar el tamaño de los puntos
-  labs(
-    title = "Número de grafos conexos según n y p",
-    x = "Probabilidad de fallo (p)",
-    y = "Número de grafos conexos",
-    color = "Número de nodos (n)"
-  ) +
-  scale_color_brewer(palette = "RdBu") +
-  theme_minimal()
+# Obtener la lista de archivos CSV en el directorio
+file_list <- list.files("../data", pattern = "*.csv", full.names = TRUE)
+
+# Iterar sobre cada archivo
+for (file in file_list) {
+  # Cargar el archivo CSV
+  data <- read.csv(file, sep = ";")
+  
+  # Extraer el nombre del archivo sin la extensión
+  file_name <- tools::file_path_sans_ext(basename(file))
+  
+  # Crear el gráfico
+  plot <- ggplot(data, aes(x = p, y = connectedGraphs, color = as.factor(n))) +
+    geom_line(size = 1) +  # Ajustar el grosor de la línea
+    geom_point(size = 2) + # Ajustar el tamaño de los puntos
+    labs(
+      title = paste("Número de grafos conexos según n y p -", file_name),
+      x = "Probabilidad de fallo (p)",
+      y = "Número de grafos conexos",
+      color = "Número de nodos (n)"
+    ) +
+    scale_color_brewer(palette = "RdBu") +
+    theme_minimal()
+  
+  # Guardar el gráfico como PNG
+  ggsave(filename = paste0(file_name, ".png"), bg = "white", plot = plot, width = 8, height = 6)
+}
